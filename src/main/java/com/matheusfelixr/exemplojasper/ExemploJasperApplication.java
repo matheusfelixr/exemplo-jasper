740f3cc23ae1a15392a899a450d7f1e827b85f86
Matheus Felix Ribeiro <matheusfelixr@hotmail.com>
2021-05-03 17:21:48 -0300

    Initial commit

---
+++ src/main/java/com/matheusfelixr/exemplojasper/ExemploJasperApplication.java
@@ -0,0 +1,13 @@
+package com.matheusfelixr.exemplojasper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploJasperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploJasperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusfelixr/exemplojasper/ExemploJasperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusfelixr.exemplojasper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploJasperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
